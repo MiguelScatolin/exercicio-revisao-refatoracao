32bc312819f956546b6a6c19380e3e92c0fb6ca6
John Doe <scatolin@DPCMIGUELSCATOLIN.localdomain>
2020-10-18 19:36:41 -0300

    REFACT: Substituindo número mágico

--- Empregado.cpp
+++ Empregado.cpp
@@ -1,14 +1,16 @@
 #include "Empregado.hpp"
 
+const int HORAS_PADRAO = 8;
+
 double Empregado::pagamentoMes(double horasTrabalhadas)
 {
 
   double t = horasTrabalhadas;
 
   //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-  if (horasTrabalhadas > 8)
+  if (horasTrabalhadas > HORAS_PADRAO)
   {
-    double x = horasTrabalhadas - 8;
+    double x = horasTrabalhadas - HORAS_PADRAO;
     t += x / 2;
   }
   return t * salarioHora;
