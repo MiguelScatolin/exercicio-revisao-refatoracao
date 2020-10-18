dc185b9dd16d9cda7a78dfa9817bc6ead11cddce
John Doe <scatolin@DPCMIGUELSCATOLIN.localdomain>
2020-10-18 19:35:40 -0300

    REFACT: separando header de Empregado da sua implementação

--- Empregado.hpp
+++ Empregado.hpp
@@ -5,24 +5,14 @@
 #include <string>
 
 class Empregado {
-	
+
   public:
-    double salarioHora;  
-    double quotaMensalVendas;  
+    double salarioHora;
+    double quotaMensalVendas;
 
 
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
-      }
-	  return t * salarioHora;
-    }
-	
+    double pagamentoMes(double horasTrabalhadas);
+
 };
 
 #endif
